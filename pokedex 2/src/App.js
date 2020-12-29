792a1c8d004d4252be2c5e2347bb7022cf8fe86f
Linda-k-daly <linda_abdelhak@hotmail.com>
2020-12-29 15:34:06 +0100

    push on portfolio

--- pokedex 2/src/App.js
+++ pokedex 2/src/App.js
@@ -2,16 +2,17 @@ import React from "react"
 import Pokedex from "./Pokedex"
 import Pokemon from "./Pokemon"
 import { Route, Switch } from "react-router-dom";
+import mockData from "./mockData";
 
 // Etapes 1 &  2
 function App() {
   return (
     <Switch>
-        <Route exact path='/' render={(props)=> <Pokedex {...props}/>}/>
-        <Route 
-          exact path="/:pokemonId"
-          render= {(props) => <Pokemon {...props}/>}
-          />
+      <Route exact path='/' render={(props) => <Pokedex {...props} />} />
+      <Route
+        exact path="/:pokemonId"
+        render={(props) => <Pokemon {...props} />}
+      />
     </Switch>
   );
 }
